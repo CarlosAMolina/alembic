d81653787c9f3610ec66c6183aff869a314c85b7
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-07-17 01:32:56 +0200

    drop unrequired code

--- src/main.py
+++
@@ -1,5 +0,0 @@
-from alembic_api import create_migration
-
-
-if __name__ == "__main__":
-    create_migration()
--- tests/test_alembic_api.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from src.alembic_api import sum_int
-
-
-class TestFunctions(unittest.TestCase):
-    def test_sum_int_returns_expected_result(self):
-        self.assertEqual(2, sum_int(1, 1))
