5d6b0b9d94e67f96b93f58c3f515f48fad6a03d2
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-07-17 01:47:29 +0200

    add example ddls

---
+++ src/ddls/bar.sql
@@ -0,0 +1,6 @@
+CREATE TABLE bar (
+    id INT PRIMARY KEY,
+    age INT NOT NULL,
+);
+
+charset latin1
---
+++ src/ddls/foo.sql
@@ -0,0 +1,6 @@
+CREATE TABLE foo (
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+);
+
+charset latin1
