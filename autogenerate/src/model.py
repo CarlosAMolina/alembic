6e9b01b2e98fa2f099bdebc6ee9e772077180bd4
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-21 18:00:40 +0200

    feat: update autogenerate [HELENA-668]

--- autogenerate/src/model.py
+++ autogenerate/src/model.py
@@ -5,6 +5,7 @@ from sqlalchemy import MetaData
 from sqlalchemy import String
 from sqlalchemy import text
 from sqlalchemy import TIMESTAMP
+from sqlalchemy import Date
 from sqlalchemy.ext.declarative import declarative_base
 
 customers_metadata = MetaData(schema="customers")
@@ -16,5 +17,13 @@ class Customers(CustomersBase):
 
     id = Column(BigInteger, primary_key=True, server_default=text("'0'"), autoincrement=True)
     des_name = Column(String(250))
-    # date_updated = Column(TIMESTAMP, onupdate=datetime.datetime.now) # Does not work. Alembic does not create the autoincrement
-    date_updated = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")) # Does not work. Alembic generates `date_updated timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP` but i only want `date_updated timestamp null on update CURRENT_TIMESTAMP`
+    # Verificar guardar automáticamente la fecha actual
+    # date_creation_a = Column(Date, nullable=False, server_default=text("CURRENT_TIMESTAMP")) # Wrong, error when running the migration, invalid. syntax.
+    # date_creation_b = Column(Date, nullable=False, server_default=text("now()")) # Wrong, error when running the migration, invalid. syntax.
+    date_creation_c = Column(Date, nullable=False, server_default=text("(now())"))
+    # Analyze how to create `on update` without a default value. I need to create `date_updated timestamp null on update CURRENT_TIMESTAMP`
+    date_updated_a = Column(TIMESTAMP, onupdate=datetime.datetime.now) # Wrong, creates `date_updated_a TIMESTAMP NULL,`
+    date_updated_b = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")) # Wrong, creates `date_updated_b TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,`
+    # date_updated_c = Column(TIMESTAMP, server_default=text("ON UPDATE CURRENT_TIMESTAMP")) # Wrong, creates `date_updated_c TIMESTAMP NULL DEFAULT ON UPDATE CURRENT_TIMESTAMP,`. Invalid sql syntax
+    # date_updated_d = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP ON UPDATE")) # Wrong, creates `date_updated_d TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE,`. Invalid sql syntax
+    date_updated_e = Column(TIMESTAMP, server_default=text("null ON UPDATE CURRENT_TIMESTAMP")) # Ok, creates `date_updated_e timestamp null on update CURRENT_TIMESTAMP`
